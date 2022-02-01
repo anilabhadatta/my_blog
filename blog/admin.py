951a54f69dc83a7a9e9a86e63f39e93280646509
anilabhadatta <anilabhadatta@gmail.com>
2022-02-01 14:50:58 +0530

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Post, Author, Tag, Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('tags', 'author', 'date',)
+    list_display = ('title', 'author', 'date',)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'post')
+
+
+# Register your models here.
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
